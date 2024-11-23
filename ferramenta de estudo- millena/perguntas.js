a1c2b4bba4d563733d75523c839e1fac449bff23
millenafernandesoliveira <0000111845005xsp@al.educacao.sp.gov.br>
2024-11-22 21:01:25 -0300

    Add files via upload

---
+++ ferramenta de estudo- millena/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que é CSS?',
+    'É um programa de estilização',
+)
+
+criaCartao(
+    'Geografia',
+    'Em que ano foi criada a ONU?',
+    '1945',
+)
+
+criaCartao(
+    'Matemática',
+    '2+2=?',
+    '2+2=4',
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz gato em Inglês?',
+    'gato em ingles é cat',
+)
